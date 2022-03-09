32046a3192b0a5b590fa5e0a9c4b60d95212dec4
adambiel22 <51017764+adambiel22@users.noreply.github.com>
2022-03-09 10:27:02 +0100

    Adding bootstrap.

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button'
+
+function LoginPage() {
+    return (
+        <Button variant="primary">Login</Button>
+    );
+}
+
+export default LoginPage;
