0d93bd7a993e45d14b5ef69c31cafd3f37c34d22
V1OL3TF0X <73484028+V1OL3TF0X@users.noreply.github.com>
2022-03-11 14:26:35 +0100

    updated form
    changed font size on alerts, reset fields on submit
    tried (unsuccessfully, for now) to make tests work

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,54 +1,64 @@
 import React from 'react';
 import {act, fireEvent, render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
-import LoginPage from "./pages/LoginPage";
+describe("Login page" , () => {
+  it('renders login button and form', () => {
+    render(<App/>);
+    const linkElement = screen.getByTestId("login");
+    expect(screen.getByLabelText(
+        'Email', {selector: 'input'}))
+        .toBeInTheDocument();
+    expect(screen.getByLabelText(
+        'Password', {selector: 'input'}))
+        .toBeInTheDocument();
+    expect(linkElement).toBeInTheDocument();
+  });
 
-test('renders login button and form', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Log in/i);
-  expect(screen.getByLabelText(
-      'Email', {selector: 'input'}))
-      .toBeInTheDocument();
-  expect(screen.getByLabelText(
-      'Password', {selector: 'input'}))
-      .toBeInTheDocument();
-  expect(linkElement).toBeInTheDocument();
-});
-test("invalidates incorrect email format", async () => {
-  render(<App/>);
-    fireEvent.input(screen.getByPlaceholderText('email@example.com'), {
-      target: {
-        value:
-            "agna aliqua."
-      }
-    });//email with invalid format
-    fireEvent.submit(screen.getByTestId("form"));
-  //expect two errors (objects with class "invalid-feedback"`
-});
-test("invalidates empty fields", async () => {
-  render(<App/>);
-    fireEvent.submit(screen.getByTestId("form"));
-  //expect two errors (objects with class "invalid-feedback"`
-});
-test("validates correct email", async () => {
-  render(<App/>);
-  fireEvent.input(screen.getByPlaceholderText('email@example.com'), {
-    target: {
-      value:
-          "email@example.com"
-    }
-  });//email with valid format
-  fireEvent.submit(screen.getByTestId("form"));
-  //expect no error (object with class "invalid-feedback"` containing "email"
-});
-test("validates non-empty password", async () => {
-  render(<App/>);
-  fireEvent.input(screen.getByPlaceholderText('Password'), {
-    target: {
-      value:
-          "agna aliqua."
-    }
-  });//email with invalid format
-  fireEvent.submit(screen.getByTestId("form"));
-  //expect no error (object with class "invalid-feedback"` containing "password"
+  it("invalidates incorrect email format", () => {
+    render(<App/>);
+    const emailInput = screen.getByTestId("email-input");
+    act(() => {
+      userEvent.type(emailInput, "testmailcom");
+      //email with invalid format
+      screen.getByTestId("login").dispatchEvent(new MouseEvent('click', {bubbles: true}));
+    });
+    //const errorMsg = screen.getByText("Email is invalid");
+    //expect(errorMsg).toBeInTheDocument()  ;
+  });
+
+  it("invalidates empty fields", () => {
+    render(<App/>);
+    act(()=> {
+      fireEvent.submit(screen.getByTestId("form"));
+    });
+    //expect two errors (objects with class "invalid-feedback"`
+  });
+  test("validates correct email", async () => {
+    render(<App/>);
+    act(()=> {
+      fireEvent.input(screen.getByPlaceholderText('email@example.com'), {
+        target: {
+          value:
+              "email@example.com"
+        }
+      });//email with valid format
+      fireEvent.click(screen.getByTestId("login"));
+    });
+
+    //expect no error (object with class "invalid-feedback"` containing "email"
+  });
+  it("validates non-empty password", async () => {
+    render(<App/>);
+    act(()=> {
+      fireEvent.input(screen.getByPlaceholderText('Password'), {
+        target: {
+          value:
+              "agna aliqua."
+        }
+      });//email with invalid format
+      fireEvent.submit(screen.getByTestId("form"));
+    });
+    //expect no error (object with class "invalid-feedback"` containing "password"
+  });
 });
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-// import {Button, Form, Container} from 'react-bootstrap'
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
@@ -10,7 +9,11 @@ type UserLoginForm = {
 };
 
 function LoginPage() {
-
+    const onChangeHandler= (field: string)=>(e: any)=>setformdata({...formdata,[field]:e.target.data})
+    const [formdata, setformdata] = useState({
+        email:"",
+        password:"",
+    });
     const validationSchema = Yup.object().shape({
         email: Yup.string()
             .required('Email is required')
@@ -22,15 +25,23 @@ function LoginPage() {
         register,
         handleSubmit,
         formState: {errors},
+        reset,
     } = useForm<UserLoginForm>({
-        resolver: yupResolver(validationSchema)
+        resolver: yupResolver(validationSchema),
+        defaultValues:
+            {
+                email: "",
+                password: ""
+            }
     });
     const onSubmit = (data: UserLoginForm) => {
         console.log(JSON.stringify(data, null, 2));
+        reset();
     };
     return (
         <div className="LoginPage">
             <header className="LoginPage-header">
+                <text>Log in as a doctor</text>
                 <form onSubmit={handleSubmit(onSubmit)} className="form-container" data-testid="form">
                     <div className="form-group">
                         <label className="form-label text-sm-start " id="email">Email</label>
@@ -39,10 +50,11 @@ function LoginPage() {
                             type="text"
                             placeholder="email@example.com"
                             {...register('email')}
+                            data-testid="email-input"
                             className={`form-control ${errors.email ? 'is-invalid' : ''}`}
                         />
                         {errors.email ?
-                        <div className="invalid-feedback text-sm-start text-small" >{errors.email?.message}</div>:
+                        <div className="invalidfeedback text-sm-center text-small" id="emailError">{errors.email?.message}</div>:
                         <div className="gap"/>}
                     </div>
                     <div className="form-group">
@@ -52,14 +64,15 @@ function LoginPage() {
                             placeholder="Password"
                             type="password"
                             {...register('password')}
+                            data-testid="password-input"
                             className={`form-control ${errors.password ? 'is-invalid' : ''}`}
                         />
                         {errors.password ?
-                        <div className="invalid-feedback text-sm-start text-small" >{errors.password?.message}</div>:
+                        <div className="invalidfeedback text-sm-center text-small" id="passwordError">{errors.password?.message}</div>:
                         <div className="gap"/>}
                     </div>
                     <div className="form-group">
-                        <button type="submit" className="btn btn-primary" name="login">
+                        <button type="submit" className="btn btn-primary" data-testid="login">
                             Log in
                         </button>
                     </div>
