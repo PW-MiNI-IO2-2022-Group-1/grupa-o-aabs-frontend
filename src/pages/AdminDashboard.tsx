d44f8de9bfe27f2cb488e46fbc5a71065446d995
adambiel22 <51017764+adambiel22@users.noreply.github.com>
2022-05-30 10:06:04 +0200

    patients table in admin dashboard

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -1,21 +1,23 @@
-import { useAuth } from '../components/AuthComponents';
+import {useAuth} from '../components/AuthComponents';
 import 'bootstrap';
-import { Container, Table, Button } from 'react-bootstrap';
+import {Container, Table, Button} from 'react-bootstrap';
 import * as Icons from 'react-bootstrap-icons';
-import { Doctor } from '../types/users';
-import { useEffect, useState } from 'react';
-import { useEditDoctorModal } from '../components/modals//useEditDoctorModal';
-import { useAddDoctorModal } from '../components/modals/useAddDoctorModal';
-import { useSimpleModal } from '../components/modals/useSimpleModal';
-import { NewDoctorData } from '../types/adminAPITypes';
+import {Doctor, Patient} from '../types/users';
+import {useEffect, useState} from 'react';
+import {useEditDoctorModal} from '../components/modals//useEditDoctorModal';
+import {useAddDoctorModal} from '../components/modals/useAddDoctorModal';
+import {useSimpleModal} from '../components/modals/useSimpleModal';
+import {NewDoctorData} from '../types/adminAPITypes';
 import * as API from '../logic/adminAPI';
-import { UnauthorizedRequestError } from '../types/requestErrors';
-import { logOut } from '../logic/login';
+import {UnauthorizedRequestError} from '../types/requestErrors';
+import {logOut} from '../logic/login';
 
 function AdminDashboard(): JSX.Element {
     const auth = useAuth();
     const [doctors, setDoctors] = useState<Doctor[]>([]);
+    const [patients, setPatients] = useState<Patient[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
+    const [patientsOrDoctors, setPatientsOrDoctors] = useState<'patients' | 'doctors'>('doctors')
 
     const [page, setPage] = useState<number>(1);
     const [pageNumber, setPageNumber] = useState<number>(1);
@@ -25,7 +27,7 @@ function AdminDashboard(): JSX.Element {
     const [showAddModal, renderAddModal] = useAddDoctorModal();
 
     const handleApiError = (reason: any) => {
-        if(reason instanceof UnauthorizedRequestError)
+        if (reason instanceof UnauthorizedRequestError)
             showErrorModal('Error', 'You are not authorized', () => logOut(auth))
         else
             showErrorModal('Error', 'Unexpected error');
@@ -39,28 +41,40 @@ function AdminDashboard(): JSX.Element {
         }).catch(handleApiError);
     }
 
+    const loadPatients = () => {
+        API.getPatients(auth, page).then(pair => {
+            setPatients(pair[0]);
+            setPageNumber(pair[1].totalPages);
+            setLoading(false);
+        }).catch(handleApiError);
+    }
+
     const deleteDoctor = (doctor: Doctor) => {
         setLoading(true);
         API.deleteDoctor(auth, doctor).catch(handleApiError)
-           .then(() => loadDoctors());
+            .then(() => loadDoctors());
     }
 
     const editDoctor = (doctor: Doctor) => {
         setLoading(true);
         API.editDoctor(auth, doctor).catch(handleApiError)
-           .then(() => loadDoctors());
+            .then(() => loadDoctors());
     }
 
     const createDoctor = (doctorData: NewDoctorData) => {
         setLoading(true);
         API.createDoctor(auth, doctorData).catch(handleApiError)
-           .then(() => loadDoctors());
+            .then(() => loadDoctors());
     }
 
     useEffect(() => {
         setLoading(true);
-        loadDoctors();
-    }, [page]);
+        if (patientsOrDoctors === 'doctors') {
+            loadDoctors();
+        } else {
+            loadPatients();
+        }
+    }, [page, patientsOrDoctors]);
 
     const renderDoctorRow = (doctor: Doctor) => {
         return (<tr id={`row-${doctor.id}`} key={`row-${doctor.id}`}>
@@ -69,13 +83,27 @@ function AdminDashboard(): JSX.Element {
             <td style={{width: '350px'}}>{doctor.email}</td>
             <td style={{width: '100px'}}>
                 <Icons.PencilSquare className='text-primary modifyBtn'
-                 style={{cursor: 'pointer'}}
-                 onClick={() => showEditModal(doctor, editDoctor)}/>
+                                    style={{cursor: 'pointer'}}
+                                    onClick={() => showEditModal(doctor, editDoctor)}/>
                 <Icons.Trash3Fill className='text-danger deleteBtn'
-                 id={'delete' + doctor.id + 'Btn'}
-                 style={{cursor: 'pointer'}}
-                 onClick={() => deleteDoctor(doctor)}/>
+                                  id={'delete' + doctor.id + 'Btn'}
+                                  style={{cursor: 'pointer'}}
+                                  onClick={() => deleteDoctor(doctor)}/>
             </td>
+        </tr>);
+    }
+
+    const renderPatientRow = (patient: Patient) => {
+        return (<tr id={`row-${patient.id}`} key={`row-${patient.id}`}>
+            <td style={{width: '350px'}}>{patient.firstName}</td>
+            <td style={{width: '350px'}}>{patient.lastName}</td>
+            <td style={{width: '350px'}}>{patient.email}</td>
+            <td style={{width: '350px'}}>{patient.pesel}</td>
+            <td style={{width: '350px'}}>{patient.address.city}</td>
+            <td style={{width: '350px'}}>{patient.address.zipCode}</td>
+            <td style={{width: '350px'}}>{patient.address.street}</td>
+            <td style={{width: '350px'}}>{patient.address.houseNumber}</td>
+            <td style={{width: '350px'}}>{patient.address.localNumber}</td>
         </tr>);
     }
 
@@ -86,15 +114,15 @@ function AdminDashboard(): JSX.Element {
             <td></td>
             <td>
                 <Icons.PersonPlusFill className='text-primary'
-                  id = 'addNewDoctorBtn'
-                  style={{cursor: 'pointer'}}
-                  onClick={() => showAddModal(createDoctor)}/>
+                                      id='addNewDoctorBtn'
+                                      style={{cursor: 'pointer'}}
+                                      onClick={() => showAddModal(createDoctor)}/>
             </td>
         </tr>)
     }
 
     const modifyPage = (delta: number) => {
-        if(page + delta >= 1 && page + delta <= pageNumber)
+        if (page + delta >= 1 && page + delta <= pageNumber)
             setPage(page => page + delta);
         return false;
     }
@@ -103,7 +131,7 @@ function AdminDashboard(): JSX.Element {
         const visiblePages: number[] = [];
         const minPage = Math.max(1, page - 3);
         const maxPage = Math.min(pageNumber, page + 3);
-        for(let i = minPage; i <= maxPage; i++)
+        for (let i = minPage; i <= maxPage; i++)
             visiblePages.push(i);
         return (<nav className='d-flex justify-content-center'>
             <ul className='pagination'>
@@ -114,10 +142,10 @@ function AdminDashboard(): JSX.Element {
                 {visiblePages.map(x => {
                     return (<li style={{cursor: 'pointer'}} className={'page-item' + (page == x ? ' active' : '')}>
                         <a className='page-link'
-                            onClick={() => {
-                                setPage(x);
-                                return false;
-                            }}
+                           onClick={() => {
+                               setPage(x);
+                               return false;
+                           }}
                         >{x}</a>
                     </li>);
                 })}
@@ -128,33 +156,79 @@ function AdminDashboard(): JSX.Element {
         </nav>);
     }
 
+    const changeToDoctorsOrPatients = () => {
+        setPage(1)
+        if (patientsOrDoctors === 'doctors') {
+            setPatientsOrDoctors('patients')
+        } else {
+            setPatientsOrDoctors('doctors')
+        }
+    }
+
+    const renderDoctorTable = () => {
+        return (
+            <Table bordered className='text-center'>
+                <thead className='table-dark'>
+                <tr>
+                    <th colSpan={4}>Doctors</th>
+                </tr>
+                </thead>
+                <thead className='table-dark'>
+                <tr>
+                    <th scope='col'>First name</th>
+                    <th scope='col'>Last name</th>
+                    <th scope='col'>Email</th>
+                    <th scope='col'>Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                {doctors.map(renderDoctorRow)}
+                {renderAddNewDoctorRow()}
+                </tbody>
+            </Table>
+        )
+    }
+
+    const renderPatientTable = () => {
+        return (
+            <Table bordered className='text-center'>
+                <thead className='table-dark'>
+                <tr>
+                    <th colSpan={9}>Patients</th>
+                </tr>
+                </thead>
+                <thead className='table-dark'>
+                <tr>
+                    <th scope='col'>First name</th>
+                    <th scope='col'>Last name</th>
+                    <th scope='col'>Email</th>
+                    <th scope='col'>PESEL</th>
+                    <th scope='col'>City</th>
+                    <th scope='col'>Zip Code</th>
+                    <th scope='col'>Street</th>
+                    <th scope='col'>House number</th>
+                    <th scope='col'>Local number</th>
+                </tr>
+                </thead>
+                <tbody>
+                {patients.map(renderPatientRow)}
+                </tbody>
+            </Table>
+        )
+    }
+
     return (<>
         {renderEditModal()}
         {renderAddModal()}
         {renderErrorModal()}
-        {loading && 
+        {loading &&
             <div className='spinner-border text-large'
-                style={{width: '100px', height: '100px'}} id='loadingIndicator'></div>}
+                 style={{width: '100px', height: '100px'}} id='loadingIndicator'></div>}
         {!loading && <Container>
-            <Table bordered className='text-center'>
-                <thead className='table-dark'>
-                    <tr>
-                        <th colSpan={4}>Doctors</th>
-                    </tr>
-                </thead>
-                <thead className='table-dark'>
-                    <tr>
-                        <th scope='col'>First name</th>
-                        <th scope='col'>Last name</th>
-                        <th scope='col'>Email</th>
-                        <th scope='col'>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {doctors.map(renderDoctorRow)}
-                    {renderAddNewDoctorRow()}
-                </tbody>
-            </Table>
+            <Button onClick={changeToDoctorsOrPatients} style={{margin: '20px'}}>
+                {'Show ' + (patientsOrDoctors === 'doctors' ? 'patients' : 'doctors')}
+            </Button>
+            {patientsOrDoctors === 'doctors' ? renderDoctorTable() : renderPatientTable()}
             {renderPaginationMenu()}
         </Container>}
     </>);
